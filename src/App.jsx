0d1a92ec43f7b72670c6a3b11a257ca5856cc320
axe-codes <dipesh.sedani@gmail.com>
2024-03-08 22:36:49 +0530

    Basic app structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MainFooter from "./componants/MainFooter";
+import MainHeader from "./componants/MainHeader";
+import MainSideBar from "./componants/MainSideBar";
+import CreatePost from "./componants/CreatePost";
+
+import PostList from "./componants/PostList";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedTab, setSelectedTab] = useState("Home");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app-container">
+      <MainSideBar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      <div className="main-container">
+        <MainHeader />
+        {selectedTab == "Home" ? <PostList /> : <CreatePost />}
+
+        <MainFooter />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
